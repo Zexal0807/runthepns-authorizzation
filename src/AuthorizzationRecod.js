18860ffbe356820f6f982dea2a6e94bcba500c7a
Zexal0807 <gallinar00@gmail.com>
2022-02-16 09:22:36 +0100

    Add default function in AuthorizzationRecord

--- src/AuthorizzationRecod.js
+++ src/AuthorizzationRecod.js
@@ -1,7 +1,18 @@
 module.exports = class AuthorizzationRecord {
+	check = (session, req) => {
+		return session == undefined;
+	};
+	success = () => {
+		console.log('Authorizzation success');
+	};
+	error = () => {
+		console.log('Authorizzation fail');
+		return 'You are not logged';
+	};
+
 	constructor({ check, success, error }) {
-		this.check = check;
-		this.success = success;
-		this.error = error;
+		this.check = check || this.check;
+		this.success = success || this.success;
+		this.error = error || this.error;
 	}
 };
